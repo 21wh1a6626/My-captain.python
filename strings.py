b6a2e074b318a310f908d26205a9d810a3295e26
21wh1a6626 <96616836+21wh1a6626@users.noreply.github.com>
2022-05-03 17:20:39 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,139 @@
+print("Hello , Poojitha")
+print('Hello , Poojitha')
+
+
+a = """Python is the most widely used programming language,
+it's easy to learn,
+used in many applications."""
+print(a)
+
+
+a = '''Python is the most widely used programming language,
+it's easy to learn,
+used in many applications.'''
+print(a)
+
+
+print("Hello , Poojitha")
+print(a[1])
+
+
+
+
+for x in "Hyderabad":
+  print(x)
+
+
+x = "Hyderabad"
+print(len(x))
+
+
+txt = "Prevention is better than cure!"
+if "cure" in txt:
+  print("Yes, 'cure' is present.")
+
+
+txt = "Prevention is better than cure!"
+print("worst" not in txt)
+
+
+txt = "Prevention is better than cure!"
+if "worst" not in txt:
+  print("No, 'worst' is NOT present.")
+
+for x in "Hyderabad":
+  print(x)
+
+
+x = "Hyderabad"
+print(len(x))
+
+
+txt = "Prevention is better than cure!"
+if "cure" in txt:
+  print("Yes, 'cure' is present.")
+
+
+txt = "Prevention is better than cure!"
+print("worst" not in txt)
+
+
+txt = "Prevention is better than cure!"
+if "worst" not in txt:
+  print("No, 'worst' is NOT present.")
+
+
+b = "Hello , Poojitha!"
+print(b[2:5])
+
+
+b = "Hello , Poojitha!"
+print(b[:5])
+
+
+b = "Hello , Poojitha!"
+print(b[2:])
+
+
+b = "Hello , Poojitha!"
+print(b[-5 : -2])
+
+
+a = "Hello, Poojitha!"
+print(a.upper())
+
+
+a = "Hello, Poojitha!"
+print(a.lower())
+
+
+a = " Hello, Poojitha! "
+print(a.strip())
+
+
+a = "Hello, Poojitha!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, Poojitha!"
+print(a.split(","))
+
+
+
+a = "Hello"
+b = "Poojitha"
+c = a + b
+print(c)
+
+
+a = "Hello"
+b = "Poojitha"
+c = a + " " + b
+print(c)
+
+
+
+age = 18
+txt = "My name is Poojitha, I am " + age
+print(txt)
+
+
+
+age = 18
+txt = "My name is Poojitha, and I am {}"
+print(txt.format(age))
+
+
+a = "Hello, Poojitha!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, Poojitha!"
+print(a.split(","))
+
+
+
+
+
+
+
