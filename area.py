83982981ec77810760dafe26d88b8748d1ca2e38
21wh1a6626 <96616836+21wh1a6626@users.noreply.github.com>
2022-05-03 18:50:46 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,4 @@
+#task1
 import math
 
 radius = float(input("Enter the radius of the circle : "))
@@ -5,3 +6,14 @@ radius = float(input("Enter the radius of the circle : "))
 area = math.pi * radius * radius
 
 print("Area of the circle is : {0}".format(area))
+
+
+
+#tas2
+filename = input("Write the filename")
+
+file_extns = filename.split(".")
+
+print("The extension is " +repr(file_extns[-1]))
+          
+
