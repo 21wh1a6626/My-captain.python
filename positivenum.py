43853d9487ba64260930ff8ec95f9ad609d793d5
21wh1a6626 <96616836+21wh1a6626@users.noreply.github.com>
2022-05-05 19:20:33 +0530

    Create positivenum.py

---
+++ positivenum.py
@@ -0,0 +1,16 @@
+list1= list(map(int,input().split()))
+for i in range (0,len(list1)):
+    a= list1[i]
+    if a>=0 :
+            print(a,end=" ")
+    else :
+        continue
+
+
+list2= list(map(int,input().split()))
+for i in range (0,len(list2)):
+    a= list2[i]
+    if a>=0 :
+            print(a,end=" ")
+    else :
+        continue
